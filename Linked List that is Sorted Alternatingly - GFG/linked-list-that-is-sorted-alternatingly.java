b3fcfb143319f1e1ed8640fbb5ccb9b68f27a99a
Priyanshu Agarwal <109196864+priyanshu-101@users.noreply.github.com>
2023-02-06 18:55:41 +0530

    Added solution - LeetHub

---
+++ Linked List that is Sorted Alternatingly - GFG/linked-list-that-is-sorted-alternatingly.java
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    
+    public Node (int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class GFG {
+    static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+	public static void main (String[] args) {
+		Scanner sc  = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0){
+		    int n = sc.nextInt();
+		    
+		    Node head = new Node(sc.nextInt());
+		    Node tail = head;
+		    
+		    for(int i=1; i<n; i++){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		    Solution obj = new Solution();
+		    head = obj.sort(head);
+		    printList(head);
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+/*
+Structure of node class is:
+
+class Node {
+    int data;
+    Node next;
+    
+    public Node (int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+class Solution {
+    
+   public Node sort(Node head){
+        //your code here, return the head of the sorted list
+        Node temp=head;
+        int count=0;
+        while(temp!=null){
+            count++;
+            temp=temp.next;
+        }
+        temp=head;
+        int a[]=new int[count];
+        count=0;
+        while(temp!=null){
+            a[count++]=temp.data;
+            temp=temp.next;
+        }
+        temp=head;
+        Arrays.sort(a);
+        int k=0;
+        while(temp!=null){
+            temp.data=a[k++];
+            temp=temp.next;
+        }
+        return head;
+   }
+
+
+}
